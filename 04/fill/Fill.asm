dd8bf19e4ae23daf0136c816116de619a52998b0
ikeno <ikenox@gmail.com>
2015-10-28 01:59:46 +0900

    initial commit

---
+++ 04/fill/Fill.asm
@@ -0,0 +1,11 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input. 
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel. When no key is pressed, the
+// program clears the screen, i.e. writes "white" in every pixel.
+
+// Put your code here.
---
+++ README.md
@@ -0,0 +1,3 @@
+# nand2tetris
+
+『[O'Reilly Japan - コンピュータシステムの理論と実装](http://www.amazon.co.jp/dp/4873117127/)』の演習用リポジトリです。
