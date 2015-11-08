49d4fdc64c80334849b28c1ab664ba86a34a1179
ikeno <ikenox@gmail.com>
2015-11-08 15:29:07 +0900

    section4 completed

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -8,4 +8,72 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+// R0: if keyboard is pushed in previous term then 1
+// R1: if keyboard is pushed in current term then 1
+
+    // define screen size
+    @8192
+    D=A
+    @number_of_pixel
+    M=D
+
+    @R0
+    M=0     // Screen is empty at the start
+(LOOP)
+    @KBD
+    D=M
+    @KEY_PUSHED
+    D;JNE   // if KBD != 0 then goto KEY_PUSHED
+(KEY_NOT_PUSHED)
+    @R1
+    M=0     // R0=0
+    @CHECK_STATE_CHANGE
+    0;JMP
+(KEY_PUSHED)
+    @R1
+    M=1     // R1=1
+(CHECK_STATE_CHANGE)    // if R0 != R1 then screen is update
+    @R0
+    D=M     // D=R0
+    @R1
+    D=D-M   // D=R0-R1
+    @LOOP
+    D;JEQ   // if D == 0 then goto LOOP
+    @i
+    M=0
+    @R1
+    D=M
+    @R0
+    M=D
+    @EMPTY_SCREEN_LOOP
+    D;JEQ
+(FILL_SCREEN_LOOP)
+    @i
+    D=M
+    @number_of_pixel
+    D=M-D
+    @LOOP
+    D;JLT
+    @SCREEN
+    A=A+D
+    M=-1
+    @i
+    MD=M+1
+    @FILL_SCREEN_LOOP
+    0;JMP
+(EMPTY_SCREEN_LOOP)
+    @i
+    D=M
+    @number_of_pixel
+    D=M-D
+    @LOOP
+    D;JLT
+    @SCREEN
+    A=A+D
+    M=0
+    @i
+    MD=M+1
+    @EMPTY_SCREEN_LOOP
+    0;JMP
+
+
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,25 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @R2
+    M=0     // R2 = 0
+    @i
+    M=1     // i = 1
+(LOOP)
+    @i
+    D=M     // D = i
+    @R1     
+    D=D-M   // D = i - R1
+    @END
+    D;JGT   // if D > 0 then goto END
+    @R0
+    D=M     // D = R0
+    @R2
+    M=D+M   // R2 += R0
+    @i
+    M=M+1   // i++
+    @LOOP
+    0;JMP   // Goto LOOP
+(END)
+    @END
+    0;JMP   // Goto END (infinite loop)
