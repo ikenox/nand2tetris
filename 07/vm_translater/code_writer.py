7a34afa8d0fc871ff7e9a6ad00e75f67a202af1b
ikeno <ikenox@gmail.com>
2016-01-18 05:02:57 +0900

    section7 completed

--- 07/vm_translater/code_writer.py
+++ 07/vm_translater/code_writer.py
@@ -1,5 +1,6 @@
 from constants import *
 
+
 class CodeWriter():
     def __init__(self, filepath):
         self.f = open(filepath, 'w')
@@ -11,8 +12,8 @@ class CodeWriter():
     def __exit__(self, exception_type, exception_value, traceback):
         self.f.close()
 
-    def set_file_name(self, file_name):
-        pass
+    def set_current_translated_file_name(self, file_name):
+        self.current_translated_file_name = file_name
 
     def write_arithmetic(self, command):
         if command == "add":
@@ -109,10 +110,13 @@ class CodeWriter():
             raise Exception("Unknown operator")
 
     def write_push_pop(self, command, segment, index):
+
+        index = int(index)
+
         if command == C_PUSH:
             if segment == "constant":
                 self.write_codes([
-                    '@%s' % str(index),
+                    '@%d' % index,
                     'D=A',
                     '@SP',
                     'A=M',
@@ -120,6 +124,131 @@ class CodeWriter():
                     '@SP',
                     'M=M+1'
                 ])
+            if segment == "local":
+                self.write_codes([
+                    '@LCL',
+                    'A=M'
+                ])
+                for i in range(index):
+                    self.write_code('A=A+1')
+                self.write_code('D=M')
+                self.write_push_from_d_register()
+            if segment == "argument":
+                self.write_codes([
+                    '@ARG',
+                    'A=M'
+                ])
+                for i in range(index):
+                    self.write_code('A=A+1')
+                self.write_code('D=M')
+                self.write_push_from_d_register()
+            if segment == "this":
+                self.write_codes([
+                    '@THIS',
+                    'A=M'
+                ])
+                for i in range(index):
+                    self.write_code('A=A+1')
+                self.write_code('D=M')
+                self.write_push_from_d_register()
+            if segment == "that":
+                self.write_codes([
+                    '@THAT',
+                    'A=M'
+                ])
+                for i in range(index):
+                    self.write_code('A=A+1')
+                self.write_code('D=M')
+                self.write_push_from_d_register()
+            if segment == "temp":
+                self.write_codes([
+                    "@%d" % TEMP_BASE_ADDRESS,
+                ])
+                for i in range(index):
+                    self.write_code('A=A+1')
+                self.write_code('D=M')
+                self.write_push_from_d_register()
+            if segment == "pointer":
+                self.write_codes([
+                    "@%d" % POINTER_BASE_ADDRESS,
+                ])
+                for i in range(index):
+                    self.write_code('A=A+1')
+                self.write_code('D=M')
+                self.write_push_from_d_register()
+            if segment == "static":
+                self.write_codes([
+                    "@%s.%d" % (self.current_translated_file_name, index),
+                ])
+                self.write_code('D=M')
+                self.write_push_from_d_register()
+
+        elif command == C_POP:
+            if segment == "local":
+                self.write_pop_to_m_register()
+                self.write_codes([
+                    'D=M',
+                    '@LCL',
+                    'A=M'
+                ])
+                for i in range(index):
+                    self.write_code('A=A+1')
+                self.write_code('M=D')
+            if segment == "argument":
+                self.write_pop_to_m_register()
+                self.write_codes([
+                    'D=M',
+                    '@ARG',
+                    'A=M'
+                ])
+                for i in range(index):
+                    self.write_code('A=A+1')
+                self.write_code('M=D')
+            if segment == "this":
+                self.write_pop_to_m_register()
+                self.write_codes([
+                    'D=M',
+                    '@THIS',
+                    'A=M'
+                ])
+                for i in range(index):
+                    self.write_code('A=A+1')
+                self.write_code('M=D')
+            if segment == "that":
+                self.write_pop_to_m_register()
+                self.write_codes([
+                    'D=M',
+                    '@THAT',
+                    'A=M'
+                ])
+                for i in range(index):
+                    self.write_code('A=A+1')
+                self.write_code('M=D')
+            if segment == "temp":
+                self.write_pop_to_m_register()
+                self.write_codes([
+                    'D=M',
+                    '@%d' % TEMP_BASE_ADDRESS,
+                ])
+                for i in range(index):
+                    self.write_code('A=A+1')
+                self.write_code('M=D')
+            if segment == "pointer":
+                self.write_pop_to_m_register()
+                self.write_codes([
+                    'D=M',
+                    '@%d' % POINTER_BASE_ADDRESS,
+                ])
+                for i in range(index):
+                    self.write_code('A=A+1')
+                self.write_code('M=D')
+            if segment == "static":
+                self.write_pop_to_m_register()
+                self.write_codes([
+                    'D=M',
+                    '@%s.%d' % (self.current_translated_file_name, index),
+                ])
+                self.write_code('M=D')
 
     def write_push_from_d_register(self):
         self.write_codes([
--- 07/vm_translater/vm_translater.py
+++ 07/vm_translater/vm_translater.py
@@ -6,6 +6,7 @@ from parser import Parser
 from code_writer import CodeWriter
 import glob
 import argparse
+import os.path
 
 
 def main():
@@ -15,11 +16,11 @@ def main():
     args = parser.parse_args()
     path = args.path
 
-    if path.endswith(".vm"):
+    if path.endswith(".vm"):    # file
         with CodeWriter(path[:-3] + ".asm") as code_writer:
             translate_file(path, code_writer)
-        print path[:-3] + ".asm"
-    else:
+        print "Translated to", path[:-3] + ".asm"
+    else:                       # directory
         if path.endswith("/"):
             path = path[:-1]
         with CodeWriter(path + ".asm") as code_writer:
@@ -27,10 +28,12 @@ def main():
             for file in files:
                 if file.endswith(".vm"):
                     translate_file(file, code_writer)
-        print path + ".asm"
+        print "Translated to", path + ".asm"
 
 
 def translate_file(file, code_writer):
+    filename, _ = os.path.splitext(os.path.basename(file))
+    code_writer.set_current_translated_file_name(filename)
     with Parser(file) as parser:
         parser.advance()
         while parser.current_command != None:
@@ -39,10 +42,9 @@ def translate_file(file, code_writer):
             elif parser.command_type() == C_PUSH:
                 code_writer.write_push_pop(C_PUSH, parser.arg1(), parser.arg2())
             elif parser.command_type() == C_POP:
-                pass
+                code_writer.write_push_pop(C_POP, parser.arg1(), parser.arg2())
 
             parser.advance()
-
 
 
 if __name__ == '__main__':
