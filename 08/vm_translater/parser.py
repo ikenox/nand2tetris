a96f773fdfb3708a08960920fd0c394f4f3c1d7c
ikeno <ikenox@gmail.com>
2016-01-18 21:09:23 +0900

    stage1 of section8 completed

---
+++ 08/vm_translater/parser.py
@@ -0,0 +1,59 @@
+from constants import *
+
+class Parser():
+    def __init__(self, filepath):
+        self.current_command = None
+        self.f_vm = open(filepath)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.f_vm.close()
+
+    def advance(self):
+        while True:
+            line = self.f_vm.readline()
+            if not line:
+                self.current_command = None
+                return self.current_command
+
+            line = line.rstrip().lstrip()
+
+            comment_i = line.find('//')
+            if comment_i != -1:
+                line = line[:comment_i]
+
+            if line != '':
+                self.current_command = line.split()
+                return self.current_command
+
+    def command_type(self):
+        if self.current_command[0] == 'push':
+            return C_PUSH
+        elif self.current_command[0] == 'pop':
+            return C_POP
+        elif self.current_command[0] == 'label':
+            return C_LABEL
+        elif self.current_command[0] == 'goto':
+            return C_GOTO
+        elif self.current_command[0] == 'if-goto':
+            return C_IF
+        elif self.current_command[0] == 'function':
+            return C_FUNCTION
+        elif self.current_command[0] == 'return':
+            return C_RETURN
+        elif self.current_command[0] == 'call':
+            return C_CALL
+        elif self.current_command[0] in ['add', 'sub', 'neg', 'eq', 'gt', 'lt', 'and', 'or', 'not']:
+            return C_ARITHMETIC
+
+    def arg1(self):
+        if self.command_type() == C_ARITHMETIC:
+            return self.current_command[0]
+        else:
+            return self.current_command[1]
+
+    def arg2(self):
+        if self.command_type() in [C_PUSH, C_POP, C_FUNCTION, C_CALL]:
+            return self.current_command[2]
---
+++ 08/vm_translater/vm_translater.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from constants import *
+from parser import Parser
+from code_writer import CodeWriter
+import glob
+import argparse
+import os.path
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('path', type=str, help='vm file or folder')
+
+    args = parser.parse_args()
+    path = args.path
+
+    if path.endswith(".vm"):  # file
+        with CodeWriter(path[:-3] + ".asm") as code_writer:
+            translate_file(path, code_writer)
+        print "Translated to", path[:-3] + ".asm"
+    else:  # directory
+        if path.endswith("/"):
+            path = path[:-1]
+        with CodeWriter(path + "/" + os.path.basename(path) + ".asm") as code_writer:
+            files = glob.glob("%s/*" % path)
+            for file in files:
+                if file.endswith(".vm"):
+                    translate_file(file, code_writer)
+        print "Translated to", path + "/" + os.path.basename(path) + ".asm"
+
+
+def translate_file(file, code_writer):
+    filename, _ = os.path.splitext(os.path.basename(file))
+    code_writer.set_current_translated_file_name(filename)
+    with Parser(file) as parser:
+        parser.advance()
+        while parser.current_command != None:
+
+            code_writer.write_code("// "+" ".join(parser.current_command))
+
+            if parser.command_type() == C_ARITHMETIC:
+                code_writer.write_arithmetic(parser.arg1())
+            elif parser.command_type() == C_PUSH:
+                code_writer.write_push_pop(C_PUSH, parser.arg1(), parser.arg2())
+            elif parser.command_type() == C_POP:
+                code_writer.write_push_pop(C_POP, parser.arg1(), parser.arg2())
+            elif parser.command_type() == C_LABEL:
+                code_writer.write_named_label(parser.arg1())
+            elif parser.command_type() == C_GOTO:
+                code_writer.write_goto(parser.arg1())
+            elif parser.command_type() == C_IF:
+                code_writer.write_if(parser.arg1())
+            elif parser.command_type() == C_FUNCTION:
+                code_writer.write_function(parser.arg1(), parser.arg2())
+            elif parser.command_type() == C_RETURN:
+                code_writer.write_return()
+            parser.advance()
+
+
+if __name__ == '__main__':
+    main()
